dcdf94f3507bb3717bcae90d89dd8f019ba14fd3
Sergen Alkan <sergen_alkan@hotmail.com>
2023-04-10 21:11:05 +0300

    10 Nisan 2023 Mola 1

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
-    //IEntity imlement eden class bir veritabanı tablosudur
+    //IEntity implement eden class bir veritabanı tablosudur
     //IEntity olması demek ben bir veritabanı kullanıyorum demek.
     //Ampulden using Entities.Abstract demek işaretleme yapmak demektir.
     public interface IEntity
