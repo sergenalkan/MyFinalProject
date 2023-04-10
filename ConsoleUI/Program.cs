9dbe25ec322e8117a5d80c94055033c799efe50f
Sergen Alkan <sergen_alkan@hotmail.com>
2023-04-10 22:37:03 +0300

    10 Nisan 2023 2. Mola

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,17 +11,33 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-                      
-            foreach (var product in productManager.GetByUnitPrice(10,100))
-            {
-                Console.WriteLine(product.ProductName);
-            }
+            // ProductTest();
 
+            //PersonelTest();
+
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void PersonelTest()
+        {
             PersonelManager personelManager = new PersonelManager(new EfPersonelDal());
             foreach (var personel in personelManager.GetAll())
             {
-                Console.WriteLine("{0} / {1} / {2}",personel.Id, personel.Name, personel.Surname);
+                Console.WriteLine("{0} / {1} / {2}", personel.Id, personel.Name, personel.Surname);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(10, 100))
+            {
+                Console.WriteLine(product.ProductName);
             }
         }
     }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -14,58 +15,8 @@ namespace DataAccess.Concrete.EntityFramework
     //default olsun diye 3.1 seçtik ve install
     //EntityFramework da bir NuGet
     //8. Gün 01.25.10 entity framework kurulumu
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-        public void Add(Product entity)
-        {
-            //IDispossable pattern implementation of c#
-            //using araştır, bu kod işin bitince sil demek
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            //dispose: iş bitince bellekten  atmak için using kullanılır
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null
-                    ? context.Set<Product>().ToList()
-                    : context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
+        
     }
 }
