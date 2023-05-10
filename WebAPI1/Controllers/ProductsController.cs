b84df36be07770989a90aaaecef9ba105e4f9ed9
Sergen Alkan <sergen_alkan@hotmail.com>
2023-05-10 03:59:21 +0300

    10.05.2023 1. Commit

---
+++ WebAPI1/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI1.Controllers
+{
+    [Route("api/[controller]")] //bu isteği yaparken bize nasıl ulaşsınlar?
+    [ApiController] //attribute -- Java(annotation) --- bilgi verme imzalama
+
+    public class ProductsController : ControllerBase
+    {
+        //Loosely Coupled - gevşek bağımlılık (soyuta bağlılık)
+        //naminf convention
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain -- bağımlılık zinciri
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
